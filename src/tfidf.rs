84f3543ddb1a5f3c94a35319b5b1cea6af4169d5
dollarkillerx <adapawang@gmail.com>
2021-10-12 18:48:53 +0800

    init project

--- src/tfidf.rs
+++ src/tfidf.rs
@@ -1,13 +1,13 @@
+use super::*;
+use serde_json;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 use std::mem;
-use serde_json;
-use super::*;
 
 #[derive(Clone)]
 struct TFIDF {
-    pb: PersistentData,
+    pd: PersistentData,
     wm: Box<WordMap>,
     dm: Box<DocMap>,
 }
@@ -15,7 +15,7 @@ struct TFIDF {
 impl TFIDF {
     pub fn new() -> TFIDF {
         TFIDF {
-            pb: PersistentData::default(),
+            pd: PersistentData::default(),
             wm: Box::new(WordMap::new()),
             dm: Box::new(DocMap::new()),
         }
@@ -25,19 +25,48 @@ impl TFIDF {
         let pd_file_handler = File::open(pd_file_name)?;
         let fd_file_handler = File::open(fd_file_name)?;
 
-        let mut pd:PersistentData = serde_json::from_reader(pd_file_handler)?;
-        let mut fd:PersistentData = serde_json::from_reader(fd_file_handler)?;
-        self.pb.doc_count = fd.doc_count;
-        self.pb.word_count = fd.word_count;
+        let mut pd: PersistentData = serde_json::from_reader(pd_file_handler)?;
+        let mut fd: PersistentData = serde_json::from_reader(fd_file_handler)?;
+        self.pd.doc_count = fd.doc_count;
+        self.pd.word_count = fd.word_count;
 
-        self.pb.docs = Vec::with_capacity(pd.docs.len());
-        self.pb.words = Vec::with_capacity(pd.words.len());
+        self.pd.docs = pd.docs.clone();
+        self.pd.words = pd.words.clone();
 
-        for (index, value) in pb.docs.enumerate() {
+        // GO GC考虑所以写下面  而RUST 可以不考虑
+        // for (index, value) in pd.docs.iter().enumerate() {
+        //     let doc = Doc{
+        //         id: value.id.clone(),
+        //         words: value.words.clone(),
+        //     };
+        //     self.pd.docs[index] = doc;
+        // }
 
+        self.init_derived_data();
+        Ok(())
+    }
+
+    fn init_derived_data(&mut self) {
+        for (k, v) in self.pd.words.iter().enumerate() {
+            self.wm.set_word(Word {
+                doc_set: Box::from(DocSet::new()),
+                index: k,
+                value: v.clone(),
+            })
         }
 
-        Ok(())
+        for (_, v) in self.pd.docs.iter().enumerate() {
+            self.dm.set_doc(v.clone());
+            for (_, v2) in v.words.iter().enumerate() {
+                match self.wm.get_word(v2.clone()) {
+                    Some(mut r) => {
+                        r.add_doc(v.id.clone());
+                        self.wm.set_word(r);
+                    }
+                    None => continue,
+                }
+            }
+        }
     }
 }
 
@@ -102,12 +131,12 @@ impl WordMap {
 #[derive(Clone)]
 struct Word {
     value: String,
-    index: i64,
+    index: usize,
     doc_set: Box<DocSet>,
 }
 
 impl Word {
-    pub fn get_index(&self) -> i64 {
+    pub fn get_index(&self) -> usize {
         self.index
     }
 
@@ -130,6 +159,10 @@ struct DocSet {
 }
 
 impl DocSet {
+    pub fn new() -> DocSet {
+        DocSet { m: HashMap::new() }
+    }
+
     pub fn append(&mut self, str: String) {
         self.m.insert(str, true);
     }
