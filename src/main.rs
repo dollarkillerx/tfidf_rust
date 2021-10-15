669992636a66f08700106edd6e0bb95c73c37e0f
dollarkillerx <adapawang@gmail.com>
2021-10-15 13:02:11 +0800

    init project

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,21 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::http::Status;
+use rocket::response::{content, status};
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> status::Custom<content::Html<&'static str>> {
+    status::Custom(Status::Ok, content::Html("<a href = 'https://github.com/dollarkillerx/tfidf_rust'> https://github.com/dollarkillerx/tfidf_rust </a>"))
 }
 
+#[get("/check")]
+fn check() -> status::Custom<content::Json<&'static str>> {
+    status::Custom(Status::Ok, content::Json("{ \"message\": \"ready perfectly\" }"))
+}
+
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index,check]).launch();
 }
